2ffa8dc609ffb92fa984779595c2182f6db6bbca
axesthump <mrsalamon77@gmail.com>
2023-03-31 14:40:12 +0300

    [GOHW-6]: поправил замечания

--- loms/migrations/20230329112432_init_outbox_orders.sql
+++ loms/migrations/20230329112432_init_outbox_orders.sql
@@ -4,10 +4,11 @@ CREATE TABLE IF NOT EXISTS outbox_orders
 (
     id          serial primary key,
     order_id    bigint not null,
-    status      int    not null,
+    status      int    not null, -- 0 - UnknownStatus. 1 - Open. 2 - InProgress. 3 - Closed.
     send_status int    not null default 1,
     err_message text   not null default ''
 );
+
 
 -- +goose StatementEnd
 
